13cedfe9c1d258c723420e294090bdb7313039a1
Gleb Pimenov <75137969+GarlicRoll@users.noreply.github.com>
2023-10-03 14:47:31 +0300

    Update oslab1_linux.cpp
    reading and writing per blocks. measuring time.

--- oslab1_linux.cpp
+++ oslab1_linux.cpp
@@ -1,74 +1,175 @@
+//
+// Created by gleb on 03.10.23.
+//
+
+#include <aio.h>
+#include <fcntl.h>
 #include <iostream>
 #include <string>
-#include <vector>
-#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <chrono>
 #include <fcntl.h>
 #include <unistd.h>
-#include <cerrno>
-#include <cstring>
+#include <signal.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <strings.h>
+#include <cassert>
 
-struct OpenFileArgs {
-    std::string file_path;
-};
+using namespace std;
 
-void* open_and_create_file(void* args) {
-    OpenFileArgs* file_args = static_cast<OpenFileArgs*>(args);
-    int fd = open(file_args->file_path.c_str(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    if (fd == -1) {
-        perror(("Failed to create/open file: " + file_args->file_path).c_str());
-        return nullptr;
-    } else {
-        std::cout << "File created/opened: " << file_args->file_path << std::endl;
-        close(fd);
-    }
-    return nullptr;
+
+string getFileName() {
+    string inputName;
+    cout << "Enter file name (/home/username/folder/file.txt): ";
+    cin >> inputName;
+    return inputName;
 }
 
-void open_and_create_files_simultaneously(const std::vector<std::string>& file_paths) {
-    std::vector<pthread_t> thread_ids(file_paths.size());
-    std::vector<OpenFileArgs> thread_args(file_paths.size());
-
-    // Launch a separate thread for each file
-    for (size_t i = 0; i < file_paths.size(); ++i) {
-        thread_args[i].file_path = file_paths[i];
-        if (pthread_create(&thread_ids[i], nullptr, open_and_create_file, &thread_args[i]) != 0) {
-            perror("Failed to create thread");
-            exit(1);
-        }
-    }
-
-    // Wait for all threads to complete
-    for (size_t i = 0; i < file_paths.size(); ++i) {
-        pthread_join(thread_ids[i], nullptr);
-    }
+void getLastError() {
+    const char *errorString = strerror(errno);
+    cerr << "Error: " << errorString << endl;
 }
 
-void printError() {
-    const char *error_string = strerror(errno);
-    std::cerr << "Error: " << error_string << std::endl;
+int openFile(string *fileName){
+    int file;
+    file = open(reinterpret_cast<const char *>(fileName), O_RDONLY|O_NONBLOCK, 0666);
+    if (file == -1) {
+        getLastError();
+    }
+    else {
+        cout << "File open successfully!" << endl;
+    }
+    return file;
 }
+
+int createFile(string *file_name) {
+    int file;
+    file = open(reinterpret_cast<const char *>(file_name), O_CREAT | O_WRONLY | O_TRUNC | O_NONBLOCK, 0666);
+    if (file == -1) {
+        getLastError();
+    }
+    else {
+        cout << "File create successfully!" << endl;
+    }
+    return file;
+}
+
 
 int main() {
 
 
-    int input = -1;
-    std::string input_string;
-    std::string input_string_2;
-    while (input != 0) {
-        std::cout << "1 - Open\\creating files\n"
-                     "0 - Exit" << std::endl;
-        std::cin >> input;
-        switch (input) {
-            case 1:
-                std::vector<std::string> file_paths = {
-                        "file1.txt", "file2.txt", "file3.txt"
-                };
-                // Call the function on the list of files
-                open_and_create_files_simultaneously(file_paths);
-                printError();
-                break;
-        }
+    ///////////////
+    ////////////// Reading
+    //////////////
 
-        return 0;
+
+
+    int blockSize = 40960;
+    cout << "Enter block size (in bytes): ";
+    cin >> blockSize;
+
+
+    int file = openFile((string *) getFileName().c_str());
+    struct stat st;
+    if (fstat(file, &st) == -1) {
+        perror("fstat");
+        return 1;
     }
+    off_t fileSize = st.st_size;
+
+    int nBlocks = fileSize / blockSize;
+    if (fileSize % blockSize != 0) {
+        ++nBlocks;
+    }
+
+    vector<aiocb> blocks(nBlocks);
+    vector<char*> buffers(nBlocks);
+
+    auto start1 = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < nBlocks; i++) {
+        aiocb readBlock{};
+        char* readBuffer = new char[blockSize];  // dynamically allocated
+        memset(readBuffer, ' ', blockSize);      // fill the buffer with spaces
+
+        readBlock.aio_fildes = file;
+        readBlock.aio_buf = readBuffer;          // assign the allocated buffer
+        readBlock.aio_nbytes = blockSize;
+        readBlock.aio_offset = i * blockSize;
+
+        buffers[i] = readBuffer;                 // save the pointer to the buffer
+        blocks[i] = readBlock;
+
+
+    }
+
+    for (int i = 0; i < nBlocks; i++) {
+        if (aio_read(&blocks[i]) == -1) {
+            perror("aio_read");
+            exit(1);
+        }
+    }
+
+    //waitForAll(blocks); // do i need it
+    // Wait for all I/O operations to complete
+    for (int i = 0; i < nBlocks; i++) {
+        const struct aiocb *aiocb_list[] = { &blocks[i] };
+        aio_suspend(aiocb_list, nBlocks, NULL);
+    }
+
+    auto end1 = chrono::high_resolution_clock::now();
+
+    ///////////////////
+    /////////////////// Writing
+    //////////////////
+
+    file = createFile((string *) getFileName().c_str());
+
+    auto start2 = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < nBlocks; i++) {
+        aiocb readBlock{};
+        //char* readBuffer = new char[blockSize];  // dynamically allocated
+        //memset(readBuffer, ' ', blockSize);      // fill the buffer with spaces
+
+        readBlock.aio_fildes = file;
+        readBlock.aio_buf = blocks[i].aio_buf;          // assign the allocated buffer
+        readBlock.aio_nbytes = blockSize;
+        readBlock.aio_offset = i * blockSize;
+
+        //buffers[i] = readBuffer;                 // save the pointer to the buffer
+        blocks[i] = readBlock;
+
+    }
+
+    for (int i = 0; i < nBlocks; i++) {
+        if (aio_write(&blocks[i]) == -1) {
+            perror("aio_write");
+            exit(1);
+        }
+    }
+
+    //waitForAll(blocks); // do i need it
+    // Wait for all I/O operations to complete
+    for (int i = 0; i < nBlocks; i++) {
+        const struct aiocb *aiocb_list[] = { &blocks[i] };
+        aio_suspend(aiocb_list, nBlocks, NULL);
+    }
+
+    auto end2 = chrono::high_resolution_clock::now();
+
+    printf("Block Size: %i\nCount Of Blocks: %i\n", blockSize, nBlocks);
+
+    chrono::duration<double> elapsed1 = end1 - start1;
+    chrono::duration<double> elapsed2 = end2 - start2;
+    std::cout << "Coping operation took: " << elapsed1.count() + elapsed2.count() << " seconds.\n";
+
+    return 0;
 }
